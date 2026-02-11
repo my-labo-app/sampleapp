3595ea646dc212654ea2e3dc9a53e43e908a6f5c
aisu <y1u2n2a3@gmail.com>
2026-02-11 17:48:08 +0900

    service-worker.js を更新

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,26 @@
-const CACHE_NAME = "my-calendar-cache-v1";
+const CACHE_NAME = "my-calendar-cache";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./sample.css",
-  "./sample.js",
-  "./manifest.json"
-];
-
-/* インストール時 */
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  self.skipWaiting();
 });
 
-/* リクエスト時（キャッシュ優先） */
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
+/* ネット優先 */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-/* 古いキャッシュ削除 */
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
